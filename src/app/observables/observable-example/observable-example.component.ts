234f6df6a57ee61232fd1ae0d6ee294b02781e69
aymen <aymen.sellaouti@gmail.com>
2020-09-10 10:18:44 +0100

    fin observable

--- src/app/observables/observable-example/observable-example.component.ts
+++ src/app/observables/observable-example/observable-example.component.ts
@@ -8,20 +8,33 @@ import { map, filter } from 'rxjs/operators';
   styleUrls: ['./observable-example.component.css'],
 })
 export class ObservableExampleComponent implements OnInit {
+  paths = ['404.png', 'as.jpg', 'cv.png'];
+  path = 'cv.png';
+  imageObservable: Observable<string>;
   constructor() {}
-
   ngOnInit(): void {
-    const observable2 = of(10, 20, 30);
-    observable2.subscribe((x) => console.log(x));
+    /*     const observable2 = of(10, 20, 30);
+    observable2.subscribe((x) => console.log(x)); */
     const observable = new Observable<number>((observer) => {
       let i = 5;
       setInterval(() => {
         if (!i) {
-          observer.error();
+          observer.complete();
         }
         observer.next(i--);
       }, 1000);
     });
+    this.imageObservable = new Observable<string>((observer) => {
+      let i = 0;
+      setInterval(() => {
+        if (i === this.paths.length) {
+          i = 0;
+        }
+        observer.next(this.paths[i++]);
+      }, 1000);
+    });
+    this.imageObservable.subscribe((path) => (this.path = path));
+    
     observable.subscribe(
       (val) => {
         console.log(val);
